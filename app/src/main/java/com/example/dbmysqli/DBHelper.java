c55cdc3f5dfcaf4951f667a39c905d82c20a86ab
Shrutika Rahatekar <53502821+Shrutika432@users.noreply.github.com>
2021-04-02 09:38:21 +0530

    till insert,update,delete

--- app/src/main/java/com/example/dbmysqli/DBHelper.java
+++ app/src/main/java/com/example/dbmysqli/DBHelper.java
@@ -2,6 +2,7 @@ package com.example.dbmysqli;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -16,7 +17,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String query = "create table users (id integer primary key autoincrement, name text, unname text, pass text)";
+        String query = "create table users (id integer primary key autoincrement, name text, uname text, pass text)";
         db.execSQL(query);
 
     }
@@ -27,19 +28,37 @@ public class DBHelper extends SQLiteOpenHelper {
         db.execSQL("drop table if exists users");
         onCreate(db);
     }
+    //insert
     public boolean insert(String name, String uname, String pass){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
         cv.put("name",name);
         cv.put("uname",uname);
-        cv.put("pas",pass);
-        long r= db.insert("User","null",cv);
+        cv.put("pass",pass);
+        long r= db.insert("users","null",cv);
         if(r==-1)
             return false;
         else
             return true;
-
-
-
+    }
+    //viewall
+    public Cursor getInfo(){
+        SQLiteDatabase db= this.getWritableDatabase();
+        Cursor cr=db.rawQuery("select * from users",null);
+        return cr;
+    }
+    //delete
+    public Boolean delete(String uname){
+        SQLiteDatabase db= this.getWritableDatabase();
+        Cursor cursor=db.rawQuery("select * from users where uname=?",new String[]{uname});
+        if(cursor.getCount()>0){
+            long r = db.delete("users","uname=?",new String[]{uname});
+            if (r==-1)
+                return false;
+            else
+                return true;
+        }
+        else
+            return false;
     }
 }
--- app/src/main/java/com/example/dbmysqli/MainActivity.java
+++ app/src/main/java/com/example/dbmysqli/MainActivity.java
@@ -1,21 +1,90 @@
 package com.example.dbmysqli;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     DBHelper dbHelper;
+    Button  b1,b2,b3,b4;
+    EditText et1,et2,et3;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         dbHelper = new DBHelper(this);
-
         SQLiteDatabase db = dbHelper.getReadableDatabase();
 
+        et1=findViewById(R.id.et1);
+        et2=findViewById(R.id.et2);
+        et3=findViewById(R.id.et3);
+        b1=findViewById(R.id.btnView);
+        b2=findViewById(R.id.btnins);
+        b3=findViewById(R.id.btnupdate);
+        b4=findViewById(R.id.btnDel);
+        //insert method
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name1=et1.getText().toString();
+                String username1=et2.getText().toString();
+                String password1=et3.getText().toString();
+                if(name1.equals("") || username1.equals("") || password1.equals("")){
+                    Toast.makeText(MainActivity.this, "Enter all the fields", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Boolean i=dbHelper.insert(name1,username1,password1);
+                    if(i==true){
+                        Toast.makeText(MainActivity.this, "Record inserted successfully", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this, "Record not inserted", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+        //view method
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor c= dbHelper.getInfo();
+                if(c.getCount()==0){
+                    Toast.makeText(MainActivity.this, "No Record Found", Toast.LENGTH_SHORT).show();
+                }
+                StringBuffer sb= new StringBuffer();
+                while (c.moveToNext()){
+                    sb.append("Name :" + c.getString(1)+"\n");
+                    sb.append("username :" + c.getString(2)+"\n");
+                    sb.append("password :" + c.getString(3)+"\n\n\n");
+
+                    AlertDialog.Builder builder= new AlertDialog.Builder(MainActivity.this);
+                    builder.setCancelable(true);
+                    builder.setTitle("Sighup user dada");
+                    builder.setMessage(sb.toString());
+                }
+            }
+        });
+        //delete
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String uname1= et2.getText().toString();
+                Boolean i= dbHelper.delete(uname1);
+                if(i==true)
+                    Toast.makeText(MainActivity.this, "Record Deleted", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "Record not deleted", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
